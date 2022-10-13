a0d2756bdd355633938a69a8ef2d345570014f62
Code Tập sự <manhchienltpt@gmail.com>
2022-10-13 18:25:56 +0700

    13/10

--- My_project/login/login.js
+++ My_project/login/login.js
@@ -1,61 +1,56 @@
 
-
-
-
-
-
-function login() {
-   let email = document.querySelector('.mail input');
-   let emailValue = email.value.trim();
-   
-   let passwordValue = document.querySelector(".password input").value.trim();
-   
-   if(isEmail(emailValue)==false) {
-      document.querySelector(".mail small ").innerHTML = "loi sai r"
-      document.querySelector(".mail small ").style.visibility = "visible"
-      document.querySelector(".mail small ").style.color = "red"  
-   } else {
-      document.querySelector(".mail small ").style.border = "1px solid green"
-   }
-   
-   if (checkPassword(passwordValue) == true) {
-      console.log('11');
-   }else {
-      console.log('22');
-   }
-   
-   
-
-
-}
-
-
 function isEmail(email) {
    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
 }
 
 
-function checkPassword(password1) {
-   
-   let userInforPasswor = localStorage.getItem("infoUser")
-   let user = JSON.parse(userInforPasswor)
-   
-   let flag =  false
-   for (i=0;i<=user.length;i++) {
-      console.log(user[i]); 
-      if (user[i].password == (password1)) {
-         flag = true
-         break
-      } else {
-         flag = false
+function login() {
+   // get input
+   let email = document.querySelector('.mail input');
+   let emailValue = email.value.trim();
+
+
+   let password = document.querySelector(".password input");
+   let passwordValue = password.value.trim();
+
+
+   //validate
+   if (isEmail(emailValue) == false) {
+      document.querySelector(".mail small ").innerHTML = "email khong chinh xac"
+      document.querySelector(".mail small ").style.visibility = "visible"
+      document.querySelector(".mail small ").style.color = "red"
+
+   }
+
+   //kiem tra do dai ky tu cua acc, pass
+   if ((passwordValue.length < 4) || (passwordValue == "")) {
+      document.querySelector(".password small ").innerHTML = "mat khau khong chinh xac"
+      document.querySelector(".password small ").style.visibility = "visible"
+      document.querySelector(".password small ").style.color = "red"
+   }
+
+
+   // check account
+   if (isEmail(emailValue) && (passwordValue.length > 4 && passwordValue !== "")) {
+      let userInforPasswor = localStorage.getItem("infoUser")
+      let user = JSON.parse(userInforPasswor)
+      
+
+      
+      let index = 0
+      for (i = 0; i < user.length; i++) {
+         if (user[i].mail == emailValue) {
+            if (user[i].password == passwordValue) {
+
+               index = i;
+               localStorage.setItem("boolean", 'true')
+               localStorage.setItem('user', index)
+               window.location.href = '/project one/index-Home.html'
+               break
+               
+            }
+         } 
       }
    }
-   if ( flag == true) {
-      return true;
-   } else {
-      return false;
-   }     
-
-   
 }
 
--- My_project/project one/main.js
+++ My_project/project one/main.js
@@ -1,3 +1,14 @@
 function dieuhuong() {
     location.assign("http://127.0.0.1:5501/project%20one/mycollection.html")
 }
+
+
+function logOut (){
+    let booleanLogOut = localStorage.getItem("boolean") 
+    let newValue = JSON.parse(booleanLogOut)
+    
+    newValue = false
+    localStorage.setItem('boolean' , JSON.stringify(newValue))
+    window.location.href = "/login/index-login.html"
+    
+}
